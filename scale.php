042a208e2cd39cc54e10254cada3acbef931c46a
unknown <2120367@talnet.nl>
2020-03-17 09:56:32 +0100

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -1,6 +1,7 @@
 <?php
 
-class Wiskunde {
+class Wiskunde 
+{
 
     function add($a, $b) {
         return $a + $b;
@@ -18,7 +19,8 @@ class Wiskunde {
     }
 }
 
-class WeegSchaal extends Wiskunde {
+class WeegSchaal extends Wiskunde 
+{
 
     public $left_weight; 
     public $right_weight; 
@@ -141,7 +143,8 @@ class WeegSchaal extends Wiskunde {
 
 }
 
-class Program {
+class Program 
+{
 
     public $inputHandler;
     public $weegSchaal;
@@ -166,7 +169,8 @@ class Program {
     }
 }
 
-class InputHandler {
+class InputHandler 
+{
 
     private $input1;
     private $input2;
